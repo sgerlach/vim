b609f68177e690b59f9d5d4189a22e439fa42ffc
Scott Gerlach <scott@scottyco.com>
2013-05-23 19:16:26 -0700

    fixed some wget issues

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+wget --no-check-certificate -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 
 cd ~/.vim/bundle
 git clone git://github.com/Lokaltog/vim-powerline.git
 git clone https://github.com/kien/ctrlp.vim.git
 mkdir -p ~/.vim/ftplugin
-curl -so ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
+wget --no-check-certificate -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
